716a5cafd37c4e2338bd842ee094ac200e2902b3
Rutvikbk Kakadiya <Rutvikbk Kakadiya>
2017-01-03 19:55:14 +0530

    use of translation()

---
+++ Annimation2/app/src/main/java/com/rutvikkakadiya/annimation2/MainActivity.java
@@ -0,0 +1,23 @@
+package com.rutvikkakadiya.annimation2;
+
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void slide(View view){
+
+        ImageView homer = (ImageView) findViewById(R.id.homer);
+
+        homer.animate().translationXBy(-1000f).setDuration(2000);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
